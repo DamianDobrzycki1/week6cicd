70c79e8d5d2784840012ec24e6db0b19fb5e319a
G00419511@atu.ie <G00419511@atu.e>
2024-10-23 15:36:29 +0100

    added, validation, Delete and Put

--- src/main/java/ie/atu/week6cicd/ProductController.java
+++ src/main/java/ie/atu/week6cicd/ProductController.java
@@ -1,5 +1,6 @@
 package ie.atu.week6cicd;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,15 +17,24 @@ public class ProductController {
         this.myProduct = myProduct;
     }
 
-      @GetMapping("/getProduct")
-        public List<Product> getAllProducts() {
-            return myProduct.getAllProduct();
-        }
-
-        @PostMapping("/newProduct")
-        public List<Product> newProduct(@RequestBody Product product)
-        {
-            //myList = myProduct.addProduct(product);
-            return myProduct.addProduct(product);
-        }
+    @GetMapping("/getProduct")
+    public List<Product> getAllProducts() {
+        return myProduct.getAllProduct();
     }
+
+    @PostMapping("/newProduct")
+    public List<Product> newProduct(@RequestBody @Valid Product product) {
+        //myList = myProduct.addProduct(product);
+        return myProduct.addProduct(product);
+    }
+    @DeleteMapping("/deleteProduct/{id}")
+    public List<Product> deleteProducts(@PathVariable int id)
+    {
+        return myProduct.deleteProducts(id);
+    }
+    @PutMapping("/editProduct/{id}")
+    public List<Product> editProducts(@Valid @PathVariable int id, @Valid @RequestBody Product product)
+    {
+        return myProduct.editProducts(id, product);
+    }
+}
