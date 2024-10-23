82486ae53da3a41479483f6e9abc1bf21e6d32c5
G00419511@atu.ie <G00419511@atu.e>
2024-10-23 14:53:19 +0100

    Added new RequestMapping, GetMapping and PostMapping code for Product

--- src/main/java/ie/atu/week6cicd/Product.java
+++ src/main/java/ie/atu/week6cicd/Product.java
@@ -1,4 +1,19 @@
 package ie.atu.week6cicd;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Product {
+
+    private int id;
+    private double price;
+    private String name;
+
 }
--- src/main/java/ie/atu/week6cicd/Week6cicdApplication.java
+++ src/main/java/ie/atu/week6cicd/Week6cicdApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Week6cicdApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(Week6cicdApplication.class, args);
     }
-
 }
